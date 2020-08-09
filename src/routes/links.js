51a69540f66514336163b284e6ea9dde0c1f8762
Binay <binayprogramming@gmail.com>
2020-08-09 17:52:36 +0200

    Updated data in BBDD

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,16 +1,19 @@
 const router = require("express").Router();
 const pool = require("../database");
 
+//ALL THE LISTS
 router.get("/", async (req, res) => {
   const links = await pool.query("SELECT * FROM links");
   console.log(links);
   res.render("links/list", { links });
 });
 
+//RENDERING ADD LISTS
 router.get("/add", (req, res) => {
   res.render("links/add");
 });
 
+//ADD LISTS
 router.post("/add", async (req, res) => {
   const { title, url, description } = req.body;
   const newLink = {
@@ -22,9 +25,30 @@ router.post("/add", async (req, res) => {
   res.redirect("/links/add");
 });
 
-router.get("/:id", async (req, res) => {
+///DELETE LISTS
+router.get("/delete/:id", async (req, res) => {
   const { id } = req.params;
   await pool.query("DELETE FROM links WHERE ID = ?", [id]);
+  res.redirect("/links");
+});
+
+//EDIT LISTS
+router.get("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const links = await pool.query("SELECT * FROM links WHERE ID = ?", [id]);
+  console.log(links);
+  res.render("links/edit", { link: links[0] });
+});
+
+router.post("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, description, url } = req.body;
+  const newLink = {
+    title,
+    description,
+    url,
+  };
+  await pool.query("UPDATE links set ? WHERE id= ?", [newLink, id]);
   res.redirect("/links");
 });
 
