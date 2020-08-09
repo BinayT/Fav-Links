3e274bb1f512aa1bdd0fbd8cf70e661ad6b20a23
Binay <binayprogramming@gmail.com>
2020-08-10 00:09:23 +0200

    User now can be added to the DB, password encypted wih the help of bcryptjs & session control with passport

---
+++ src/lib/helpers.js
@@ -0,0 +1,18 @@
+const bcryptjs = require("bcryptjs");
+const helpers = {};
+
+helpers.encryptPassort = async (password) => {
+  const salt = await bcryptjs.genSalt(10);
+  const hash = await bcryptjs.hash(password, salt); //Final Password
+  return hash;
+};
+
+helpers.matchPassword = async (password, savedPassword) => {
+  try {
+    await bcryptjs.compare(password, savedPassword);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+module.exports = helpers;
