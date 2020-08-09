1dd234cd1a834458ce7e96ee6cdab4c61dc00093
Binay <binayprogramming@gmail.com>
2020-08-09 15:16:39 +0200

    Getting the data correctly from the DDBB

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
 const pool = require("../database");
 
+router.get("/", async (req, res) => {
+  const links = await pool.query("SELECT * FROM links");
+  console.log(links);
+  res.render("links/list", { links });
+});
+
 router.get("/add", (req, res) => {
   res.render("links/add");
 });
